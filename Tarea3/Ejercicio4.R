74e322df270f7576efeac25bd8aeddeb986218db
GenaroReynoso <arigenaro27@ciencias.unam.mx>
2025-04-16 22:03:07 -0600

    Create Ejercicio4.R

---
+++ Tarea3/Ejercicio4.R
@@ -0,0 +1,13 @@
+#Librerias usadas
+library(tidyverse)
+library(FactoMineR)
+library(factoextra)
+library(psych)
+
+#Ejercicio 4 - Tarea 3
+
+#Equipo 6:
+#-Reyes Maqueda Edgar
+#-Reynoso Torres Ari Genaro
+#-Villarreal Maldonado Andre
+
